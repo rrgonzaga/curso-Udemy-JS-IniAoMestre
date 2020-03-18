8634f7beb0fc957205eaa08e449aa76f8ced4455
Rodrigo Gonzaga <rrgonzaga@gmail.com>
2020-03-18 00:12:46 -0300

    Seção 09: aula 63. Melhorar a acessibilidade do hamburguer menu
    - Usar JavaScript para melhorar a acessibilidade do hamburguer menu; 
    - Deixar o hamburguer menu mais acessível usando atributos ARIA (aria-controls e aria-expanded).
    - Uso das funções getAttribute e setAttribute.

--- aula08_desafio/javascript/menu.js
+++ aula08_desafio/javascript/menu.js
@@ -1,19 +1,26 @@
-// Seção 08: aula 62. Exercício desafio - Resolução (solução do professor)
+// Seção 08: aula 63. Usar JavaScript para melhorar a acessibilidade do hamburguer menu
 /*
-    - Implementar o comportamento correto do menu hamburguer.  
-    - Fazendo a melhoria da página permitindo que ela funcione mesmo sem JavaScript através da 
-    classe no-js.
-*/  
+    - Deixar o hamburguer menu mais acessível usando atributos ARIA (aria-controls e aria-expanded).
+    - Uso das funções getAttribute e setAttribute
+*/ 
 (function(){
         'use strict';
         var $html = document.querySelector("html");
         var $btnMenu = document.querySelector(".header-nav__hamburgger");
+
+        var ariaControl = $btnMenu.getAttribute('aria-controls');
+
+        var $menu = document.getElementById(ariaControl);
 
         $html.classList.remove("no-js");
         $html.classList.add("js");
 
         function toogleMenu() {            
             $html.classList.toggle("menu-opened");
+            var ariaExpanded = this.getAttribute('aria-expanded') === "true";
+
+            $btnMenu.setAttribute('aria-expanded',!ariaExpanded);
+            $menu.setAttribute('aria-expanded', !ariaExpanded);
         }
 
         $btnMenu.addEventListener("click", toogleMenu);
